ffcb0243f5a72897c208058f9c3243ebe7babcc4
Anisul Islam <anisulislam@Anisuls-MacBook-Pro.local>
2022-02-24 21:49:32 +0600

    working increase decrease code added using useReducer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.css';
+import Count from './Components/Count';
 
 function App() {
   return (
     <div className="App">
-      
+      <Count/>
     </div>
   );
 }
---
+++ src/Components/Count.js
@@ -0,0 +1,46 @@
+import React, { useReducer } from 'react';
+
+const countState = {
+    click:0,
+    count:0,
+    active:true
+
+}
+
+const countReducer = (state, action) =>{
+    switch (action.type) {
+        case 'INCREMENT':
+            return{
+                ...state,
+                count: state.count + 1
+            }
+            break;
+        case 'DECREMENT':
+             return{
+                ...state, 
+                count: state.count - 1
+             }
+
+        default:
+            return state;
+            break;
+    }
+}
+
+const Count = () => {
+    
+    const [state, dispatch] = useReducer(countReducer, countState);
+
+
+
+
+    return (
+        <div>
+            <h1>{state.count}</h1>
+            <button onClick={()=>dispatch({type:'INCREMENT'})}>Increase</button>
+            <button onClick={()=> dispatch({type:'DECREMENT'})}>Decrease</button>
+        </div>
+    );
+};
+
+export default Count;
